afc5d914a533435f3ca3964c3b809c6011266364
Tyler Delmus <tylerdelmus@gmail.com>
2019-06-11 21:33:45 -0500

    Updated hardcoded func offsets to 18.8.41

--- AOSharp.Core/GameData/FuncOffsets.cs
+++ AOSharp.Core/GameData/FuncOffsets.cs
@@ -9,7 +9,7 @@ namespace AOSharp.Core.GameData
         public static int IsDynelInWeaponRange = 0x679C1; // Gamecode.dll - 55 8B EC 83 EC 18 33 C0 56 8B F1 39 45 08 75 07 
         public static int IsInRange = 0x68969; // Gamecode.dll - 55 8B EC 83 EC 18 56 8B F1 8B 4E 08 E8 ? ? ? ? 8B 48 5C 89 4D E8 8B 40 60 89 45 EC 8D 45 E8 50 FF 15 ? ? ? ? 59 89 45 F8 85 C0 0F 84 ? ? ? ? 
         public static int GetDummyWeapon = 0x686D0;  // Gamecode.dll - 55 8B EC 51 56 8D 45 08 50 8B F1 8D 45 FC 50 8D 4E 0C E8 ? ? ? ? 8B 45 FC 3B 46 10 5E 74 05   
-        public static int AppendText = 0x9AA70;  // GUI.dll - B8 ? ? ? ? E8 ? ? ? ? 83 EC 70 53 56 57 8B F1 68 ? ? ? ? 8D 4D D8 FF 15 ? ? ? ? 33 DB 89 5D FC 39 5D 0C 74 6B 
-        public static int GetOptionWindow = 0xC294F; // GUI.dll - 55 8B EC 51 33 C0 39 41 10 74 10 8B 41 0C 8B 00 8D 4D FC 89 45 FC E8 ? ? ? ? 
+        public static int AppendText = 0x9B37F;  // GUI.dll - B8 ? ? ? ? E8 ? ? ? ? 83 EC 70 53 56 57 8B F1 68 ? ? ? ? 8D 4D D8 FF 15 ? ? ? ? 33 DB 89 5D FC 39 5D 0C 74 6B 
+        public static int GetOptionWindow = 0xC2E2A; // GUI.dll - 55 8B EC 51 33 C0 39 41 10 74 10 8B 41 0C 8B 00 8D 4D FC 89 45 FC E8 ? ? ? ? 
     }
 }
