d7e1033049ccabce0ceb31157e8bc8ad5543487a
Tyler Delmus <tylerdelmus@gmail.com>
2021-08-21 01:38:20 -0500

    AOSharp.Recast InitialCommit

---
+++ AOSharp.Recast/NavmeshGenerator.cs
@@ -0,0 +1,128 @@
+﻿using AOSharp.Common.GameData;
+using AOSharp.Common.Unmanaged.DbObjects;
+using AOSharp.Core;
+using AOSharp.Core.UI;
+using org.critterai.geom;
+using org.critterai.nav;
+using org.critterai.nmbuild;
+using org.critterai.nmgen;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using NavVector3 = org.critterai.Vector3;
+
+namespace AOSharp.Recast
+{
+    public class NavmeshGenerator
+    {
+        public static async Task<Navmesh> BakeAsync(NMGenParams navmeshGenParams)
+        {
+            return await Task.Run(() =>
+            {
+                List<Mesh> terrainChunks = Terrain.CreateFromTilemap(Playfield.RDBTilemap);
+                List<SurfaceResource> surfaces = Playfield.Zones.Select(x => SurfaceResource.Get(Playfield.ModelIdentity.Instance << 16 | x.Instance)).ToList();
+
+                TriangleMesh triMesh = CreateTriangleMesh(terrainChunks, surfaces);
+
+                byte[] areas = NMGen.CreateDefaultAreaBuffer(triMesh.triCount);
+                InputGeometryBuilder gbuilder = InputGeometryBuilder.Create(triMesh, areas, navmeshGenParams.WalkableSlope);
+
+                if (gbuilder == null)
+                    throw new Exception($"Failed to create InputGeometryBuilder");
+
+                gbuilder.BuildAll();
+                InputGeometry geom = gbuilder.Result;
+                TileSetDefinition tdef = TileSetDefinition.Create(geom.BoundsMin, geom.BoundsMax, navmeshGenParams, geom);
+
+                List<TileBuildAssets> tiles = new List<TileBuildAssets>();
+                int maxPolys = 0;
+
+                Navmesh navmesh = null;
+
+                Parallel.For(0, tdef.Width, x => 
+                {
+                    Parallel.For(0, tdef.Depth, z =>
+                    {
+                        IncrementalBuilder builder = IncrementalBuilder.Create(x, z, NMGenAssetFlag.PolyMesh | NMGenAssetFlag.DetailMesh, tdef, ProcessorSet.CreateStandard(ProcessorSet.StandardOptions));
+                        builder.BuildAll();
+
+                        switch (builder.State)
+                        {
+                            case NMGenState.Aborted:
+                                //Is this fatal??
+                                throw new Exception($"MultiTile Creation aborted");
+                        }
+
+                        NMGenAssets assets = builder.Result;
+                        TileBuildTask task = TileBuildTask.Create(x, z, assets.PolyMesh.GetData(false), assets.DetailMesh.GetData(false), ConnectionSet.CreateEmpty(), false, false, 0);
+                        task.Run();
+                        tiles.Add(task.Result);
+                        maxPolys = Math.Max(maxPolys, task.Result.PolyCount);
+                        Chat.WriteLine($"BuildTaskCreated ({x}, {z})");
+                    });
+                });
+
+                NavmeshParams nconfig = new NavmeshParams(tdef.BoundsMin, tdef.TileWorldSize,
+                                                          tdef.TileWorldSize, tiles.Count, maxPolys);
+
+                var status = Navmesh.Create(nconfig, out navmesh);
+                if ((status & NavStatus.Sucess) == 0)
+                    throw new Exception($"Navmesh creation was not successful. {status}");
+
+                if (navmesh == null)
+                    throw new Exception($"Navmesh is null?");
+
+                // Add the tiles to the navigation mesh.
+                foreach (TileBuildAssets tile in tiles)
+                    navmesh.AddTile(tile.Tile, Navmesh.NullTile, out _);
+
+                return navmesh;
+            });
+        }
+
+        private static TriangleMesh CreateTriangleMesh(List<Mesh> terrainChunks, List<SurfaceResource> surfaces)
+        {
+            List<NavVector3> verts = new List<NavVector3>();
+            List<int> indices = new List<int>();
+
+            var numVerts = 0;
+            foreach (var mesh in terrainChunks)
+            {
+                foreach (var i in mesh.Triangles)
+                    indices.Add(numVerts + i);
+
+                foreach (var vert in mesh.Vertices)
+                {
+                    var worldPos = mesh.LocalToWorldMatrix.MultiplyPoint3x4(vert);
+                    verts.Add(worldPos.ToCAIVector3());
+
+                    numVerts++;
+                }
+            }
+
+            foreach (var surface in surfaces)
+            {
+                if (surface == null)
+                    continue;
+
+                foreach(Mesh mesh in surface.Meshes)
+                {
+                    foreach (var i in mesh.Triangles)
+                        indices.Add(numVerts + i);
+
+                    foreach (var vert in mesh.Vertices)
+                    {
+                        verts.Add(vert.ToCAIVector3());
+                        numVerts++;
+                    }
+                }
+            }
+
+            return new TriangleMesh(verts.ToArray(), verts.Count, indices.ToArray(), indices.Count / 3);
+        }
+    }
+}
---
+++ AOSharp.Recast/Terrain.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using AOSharp.Common.GameData;
+using AOSharp.Common.Unmanaged.DbObjects;
+using AOSharp.Core;
+
+namespace AOSharp.Recast
+{
+    public class Terrain
+    {
+        public static List<Mesh> CreateFromTilemap(RDBTilemap tilemap)
+        {
+            List<Mesh> chunkMeshes = new List<Mesh>();
+
+            foreach(RDBTilemap.Chunk chunk in tilemap.Chunks)
+                chunkMeshes.Add(CreateMesh(chunk, tilemap.TileSize, tilemap.HeightmapScale));
+ 
+            return chunkMeshes;
+        }
+
+        private static Mesh CreateMesh(RDBTilemap.Chunk chunk, float tileSize, float heightMapScale)
+        {
+            var lod = 0;
+            var index = 0;
+            var triIdx = 0;
+
+            if (chunk.ChunkSize == 16)
+                lod = 0;
+
+            var sizeMultiplier = 1;
+            var physicalSize = chunk.ChunkSize / sizeMultiplier;
+
+            for (int i = 0; i < lod; i++)
+            {
+                physicalSize /= 2;
+                sizeMultiplier *= 2;
+            }
+
+            if (lod != 0)
+            {
+                physicalSize++;
+            }
+
+            var vertices = new Vector3[physicalSize * physicalSize];
+
+            for (var y = 0; y < physicalSize; y++)
+            {
+                for (var x = 0; x < physicalSize; x++)
+                {
+                    var heightPosX = (int)x * sizeMultiplier;
+                    var heightPosY = (int)y * sizeMultiplier;
+
+                    vertices[index] = new Vector3(
+                        (x * sizeMultiplier) * tileSize,
+                        (float)chunk.HeightMap[heightPosX, heightPosY] * heightMapScale,
+                        (y * sizeMultiplier) * tileSize);
+
+                    index++;
+                }
+            }
+
+            var indices = new List<int>();
+            for (int y = 0; y < physicalSize - 1; y++)
+            {
+                for (int x = 0; x < physicalSize - 1; x++)
+                {
+                    indices.Add((y * physicalSize) + x + 1);
+                    indices.Add((y * physicalSize) + x);
+                    indices.Add(((y + 1) * physicalSize) + x);
+
+                    indices.Add((y * physicalSize) + x + 1);
+                    indices.Add(((y + 1) * physicalSize) + x);
+                    indices.Add(((y + 1) * physicalSize) + x + 1);
+
+                    triIdx += 6;
+                }
+            }
+
+            return new Mesh
+            {
+                Triangles = indices,
+                Vertices = vertices.ToList(),
+                Position = new Vector3((float)(chunk.X * (chunk.ChunkSize - 1)) * tileSize, 0, (float)(chunk.Y * (chunk.ChunkSize - 1)) * tileSize),
+                Rotation = Quaternion.Identity,
+                Scale = new Vector3(1, 1, 1)
+            };
+        }
+    }
+}
