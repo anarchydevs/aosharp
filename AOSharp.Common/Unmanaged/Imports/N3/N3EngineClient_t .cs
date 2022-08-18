9b3d12150ef589d0fd72423c0ed44ebe11652ac3
Pixelmania <84938061+Malishade@users.noreply.github.com>
2022-08-18 23:14:29 +0200

    Added DropdownMenu, camera dynel

--- AOSharp.Common/Unmanaged/Imports/N3/N3EngineClient_t .cs
+++ AOSharp.Common/Unmanaged/Imports/N3/N3EngineClient_t .cs
@@ -9,6 +9,9 @@ namespace AOSharp.Common.Unmanaged.Imports
         [DllImport("N3.dll", EntryPoint = "?GetPlayfield@n3EngineClient_t@@SAPAVn3Playfield_t@@XZ", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr GetPlayfield();
 
+        [DllImport("N3.dll", EntryPoint = "?GetActiveCamera@n3EngineClient_t@@QBEPAVn3Camera_t@@XZ", CallingConvention = CallingConvention.ThisCall)]
+        public static extern IntPtr GetActiveCamera(IntPtr pThis);
+
         [DllImport("N3.dll", EntryPoint = "?GetClientControlDynel@n3EngineClient_t@@QBEPAVn3VisualDynel_t@@XZ", CallingConvention = CallingConvention.ThisCall)]
         public static extern IntPtr GetClientControlDynel(IntPtr pThis);
 
