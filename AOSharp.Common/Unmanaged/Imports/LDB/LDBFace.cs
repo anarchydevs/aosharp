9f6d32393a0c8c75ed83c5f087c312d3143e49b8
Pixelmania <84938061+Malishade@users.noreply.github.com>
2023-08-17 22:29:59 +0200

    More N3Messages deserialized

--- AOSharp.Common/Unmanaged/Imports/LDB/LDBFace.cs
+++ AOSharp.Common/Unmanaged/Imports/LDB/LDBFace.cs
@@ -7,6 +7,6 @@ namespace AOSharp.Common.Unmanaged.Imports
     public class LDBFace
     {
         [DllImport("ldb.dll", EntryPoint = "?GetText@LDBface@@SA?AV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@II@Z", CallingConvention = CallingConvention.Cdecl)]
-        public static extern StdString GetText(int type, int instance);
+        public static extern void GetText(IntPtr ptr, int type, int instance);
     }
 }
