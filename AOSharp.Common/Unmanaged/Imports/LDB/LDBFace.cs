da7d75065b8753aee7edfb1236c2b6db7d6683d2
Tyler Delmus <tylerdelmus@gmail.com>
2023-08-21 21:18:36 -0500

    Added AcceptBSInvite

--- AOSharp.Common/Unmanaged/Imports/LDB/LDBFace.cs
+++ AOSharp.Common/Unmanaged/Imports/LDB/LDBFace.cs
@@ -7,6 +7,6 @@ namespace AOSharp.Common.Unmanaged.Imports
     public class LDBFace
     {
         [DllImport("ldb.dll", EntryPoint = "?GetText@LDBface@@SA?AV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@II@Z", CallingConvention = CallingConvention.Cdecl)]
-        public static extern StdString GetText(int type, int instance);
+        public static extern IntPtr GetText(IntPtr pString, int type, int instance);
     }
 }
